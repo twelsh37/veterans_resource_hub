a5b8043f5326f3394689d60a4654cf0b6919706b
twelsh37 <twelsh37@gmail.com>
2024-10-25 12:08:17 +0100

    Updated footer for VRH

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-50 h-[50px] bg-gray-800 text-white">
       <div className="container mx-auto px-4 h-full flex items-center justify-center">
-        <p>© 2023 Your Company Name. All rights reserved.</p>
+        <p>© 2024. Veterans Resource HubAll rights reserved.</p>
       </div>
     </footer>
   )
