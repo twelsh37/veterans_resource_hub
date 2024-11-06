5b18c8892ac38a3832381d916ea3952be121af65
twelsh37 <twelsh37@gmail.com>
2024-11-06 20:11:30 +0000

    Reflecting change in global.css location

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import '@/app/styles/globals.css';
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+
 
 const inter = Inter({ subsets: ['latin'] })
 
