55b97577d6d9a1daaac283879b14d70f789d8886
twelsh37 <twelsh37@gmail.com>
2024-11-06 22:20:53 +0000

    Minor updates

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import '@/app/styles/globals.css';
+import './styles/globals.css';
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
